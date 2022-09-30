62dec1fc8ea2e7880b0bc155b1a48b3ecb522122
Andres Bastidas <andres@bastidas.us>
2022-09-29 21:07:48 -0400

    Added a simple front-end page to display the random exercise

--- src/resources/views/generate.blade.php
+++ src/resources/views/generate.blade.php
@@ -1,32 +1,123 @@
-<h1>Exercise</h1>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
 
-@if(!empty($error))
-    <h2>{{ $error }}</h2>
-@endif
+    <title>{{ config('app.name') }}</title>
 
-{{ $title }}<br>
-{{ $sub_title }}<br>
-{{ $description }}<br>
-{{ $repetitions }}<br>
-{{ $tool }}<br>
-@foreach($samples as $sample)
-    <img src="{{ $sample }}" alt="{{ $title }}">
-@endforeach
-<br>
-@php
-    $embeded = str_replace(
-        ["https://youtu.be/", "?t"],
-        ["https://www.youtube-nocookie.com/embed/", "?start"],
-        $video
-    );
-@endphp
-{{ $embeded }}<br>
-{{ $video }}<br>
+    <!-- Styles -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 
-<iframe width="560"
-        height="315"
-        src="{{ $embeded }}"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
+</head>
+<body>
+
+    <main>
+        <section>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+                </div>
+            </nav>
+        </section>
+    </main>
+
+    <main class="container mt-4">
+
+        <section>
+
+            @if(!empty($error))
+                <div class="row">
+                    <div class="col">
+                        <div class="alert-danger">{{ $error }}</div>
+                    </div>
+                </div>
+            @endif
+
+        </section>
+
+        <section>
+            <div class="row">
+                <div class="col">
+                    <h3>{{ $title }}</h3>
+                    <h4>{{ $sub_title }}</h4>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col">
+                    <p>{{ $description }}</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <p><span class="text-black">Repetitions:</span> {{ $repetitions }}</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <p><span class="text-black">Tool:</span> {{ $tool }}</p>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col">
+
+                    <h5>Sample Images</h5>
+                    <ul class="list-group">
+                    @foreach($samples as $sample)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            <img class="img-fluid" src="{{ $sample }}" alt="{{ $title }}">
+                        </li>
+                    @endforeach
+                    </ul>
+
+                </div>
+            </div>
+
+            @php
+                $embed = str_contains($video, "youtu");
+                $embeded = str_replace(
+                    ["https://youtu.be/", "?t"],
+                    ["https://www.youtube-nocookie.com/embed/", "?start"],
+                    $video
+                );
+            @endphp
+
+            <div class="row mt-3">
+                <div class="col">
+                    @if ($embed)
+                        <h5>Preview Video</h5>
+                        <div class="youtube-video-container">
+                            <iframe width="560"
+                                    height="315"
+                                    src="{{ $embeded }}"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    @else
+                        <a href="{{ $video }}">Preview Video</a>
+                    @endif
+                </div>
+            </div>
+
+        </section>
+
+        <section>
+            <div class="row mt-4">
+                <p class="text-black-50 small text-end"><small>Copyright {{ date('Y') }} Andres Bastidas</small></p>
+            </div>
+        </section>
+
+    </main>
+</body>
+</html>
